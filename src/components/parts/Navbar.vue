fabedd2415c8f2919f5809cf49e5b4a0a487d07b
rijal <jeryzal333@gmail.com>
2021-10-31 15:16:28 +0700

    add fontawesome

---
+++ src/components/parts/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+  <div class="px-2 mx-2 navbar-start">
+    <span class="text-lg font-bold">
+            Book of stock
+          </span>
+  </div> 
+  <div class="hidden px-2 mx-2 navbar-center lg:flex">
+    <div class="flex items-stretch">
+      <a class="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </a> 
+      <a class="btn btn-ghost btn-sm rounded-btn">
+              Portfolio
+            </a> 
+      <a class="btn btn-ghost btn-sm rounded-btn">
+              About
+            </a> 
+      <a class="btn btn-ghost btn-sm rounded-btn">
+              Contact
+            </a>
+    </div>
+  </div> 
+  <div class="navbar-end">
+      <span>Setting</span>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+}
+</script>
