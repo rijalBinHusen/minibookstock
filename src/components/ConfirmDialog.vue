4fd3891a13609ec0b134a73063464b9293e674b1
rijalBinHusen <jeryzal333@gmail.com>
2023-02-13 15:35:46 +0700

    add button to alert dialog

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -3,11 +3,37 @@
     <input checked type="checkbox" id="modal-confirm" class="modal-toggle" />
     <div id="modal-confirm" class="modal">
       <div class="modal-box">
-        <h3 class="font-bold text-lg">{{ typeDialog == 'confirm' ? 'Peringatan' : 'Pemberitahuan' }}</h3>
+        <h3 class="font-bold text-lg">
+          {{ typeDialog == 'confirm' ? 'Peringatan' : 'Pemberitahuan' }}
+        </h3>
         <p class="py-4" v-html="dialogMessage"></p>
         <div v-if="typeDialog == 'confirm'" class="modal-action">
-          <Button type="button" class="ml-2" primary small value="Iya" @trig="handleButton(true)" />
-          <Button type="button" class="ml-2" secondary small value="Tidak" @trig="handleButton(false)" />
+          <Button
+            type="button"
+            class="ml-2"
+            primary
+            small
+            value="Iya"
+            @trig="handleButton(true)"
+          />
+          <Button
+            type="button"
+            class="ml-2"
+            secondary
+            small
+            value="Tidak"
+            @trig="handleButton(false)"
+          />
+        </div>
+        <div v-else class="modal-action">
+          <Button
+            type="button"
+            class="ml-2"
+            primary
+            small
+            value="Oke"
+            @trig="handleButton(false)"
+          />
         </div>
       </div>
     </div>
@@ -15,29 +41,27 @@
 </template>
 
 <script setup>
-
 import { computed, onMounted, onBeforeUnmount } from 'vue';
 import { useStore } from 'vuex';
 import Button from './elements/Button.vue';
-import { keyPress } from '@/composables/keyEvent.js'
+import { keyPress } from '@/composables/keyEvent.js';
 
-const store = useStore()
+const store = useStore();
 // If the type of dialog is confirm
 // If the type of dialog is alert
-const typeDialog = computed(() => store.state.dialogType)
-const dialogMessage = computed(() => store.state.dialogMessage)
+const typeDialog = computed(() => store.state.dialogType);
+const dialogMessage = computed(() => store.state.dialogMessage);
 // if the button yes clicked
 // if the button no clicked or the modal closed
 const handleButton = (boolean) => {
-    store.commit('tunnelMessage', boolean)
-}
-
+  store.commit('tunnelMessage', boolean);
+};
 
 onMounted(() => {
-  window.addEventListener('keydown', keyPress )
-})
+  window.addEventListener('keydown', keyPress);
+});
 
 onBeforeUnmount(() => {
-  window.removeEventListener('keydown', keyPress )
-})
+  window.removeEventListener('keydown', keyPress);
+});
 </script>
