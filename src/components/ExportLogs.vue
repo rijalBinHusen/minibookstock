b80c753e3e73b21cf940ab761e7720c6e01a1c17
rijalBinHusen <jeryzal333@gmail.com>
2023-02-24 07:51:19 +0700

    export log by periode

--- src/components/ExportLogs.vue
+++ src/components/ExportLogs.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="mt-10">
+  <div class="mt-10 flex items-center">
+    <span class="">Tanggal mulai</span>
+    <date-picker 
+        class="m-2 bg-base-200 p-2 rounded" 
+        v-model="date"
+    >
+    </date-picker> 
     <Button
       small
       primary
@@ -12,33 +18,28 @@
 
 <script setup>
 import Button from './elements/Button.vue';
-// import all function that get all data
-import {
-  launchForm,
-  closeModalOrDialog,
-  loaderMessage,
-} from '../utils/launchForm';
-// import function to export text to file and download it
+import { launchForm, closeModalOrDialog, } from '../utils/launchForm';
 import { startExport } from '../utils/ExportAsFile';
 // import date time formater
 import { full } from '../utils/dateFormat';
 import { useIdb } from '../utils/localforage';
+import datePicker from "vue3-datepicker"
+import { ref } from 'vue';
+
+const date = ref(new Date())
 
 const handleExportLogs = async () => {
   // launch the loader
   launchForm('Loader', false);
-  // will contain all result
-  const result = [];
-  // summary db
-  const summary = await useIdb('logs');
-  // get all items summary
-  const log_items = await summary.getItems();
+  // acivitylog db
+  const acivitylog = useIdb('logs');
+  // get all items acivitylog
+  const log_items = await acivitylog.getItemsByKeyGreaterThan('time', date.value.getTime());
   // // export all data as file
   await startExport(
     log_items,
-    'Log aktivitas aplikasi FIFO ' + full() + '.json'
+    'Log aktivitas aplikasi FIFO mulai ' + full(date.value) + '.json'
   );
-  // console.log(result)
   // close the loader
   closeModalOrDialog(false);
 };
