dd36ec121eed289a482d705fc142262d8d5729ef
rijalbinhusen <jeryzal333@gmail.com>
2024-01-29 15:03:21 +0700

    test getItemById masterItem

--- src/pages/MasterItems/MasterItem.spec.js
+++ src/pages/MasterItems/MasterItem.spec.js
@@ -5,7 +5,7 @@ import { Items } from "./MasterItems";
 
 describe("Click submit button ", async () => {
   
-  const { createItem } = Items();
+  const { createItem, getItemById  } = Items();
   
   // testing create item
   it("Create new record", async () => {
@@ -13,7 +13,7 @@ describe("Click submit button ", async () => {
     const kodeItemError = "Kode item tidak boleh kosong";
     const nameItemError = "Nama item tidak boleh kosong";
     const umurItemError = "Umur item tidak boleh kosong";
-    // set value form
+    
     for(let i=0; i < 20; i++) {
       // new record
       const kd_item = faker.datatype.string(5);
@@ -42,4 +42,29 @@ describe("Click submit button ", async () => {
       expect(createFailed4).toBe(kodeItemError + ", " + nameItemError + ", " + umurItemError)
     }
   });
+
+  it("Create and getItemById, the item must matched", async () => {
+    
+    for(let i=0; i < 20; i++) {
+      // new record
+      const kd_item = faker.datatype.string(5);
+      const nm_item = faker.datatype.string(15);
+      const age_item = faker.datatype.number({ min: 1, max: 12 }) + "";
+      const sort_item = faker.datatype.number();
+      
+      // item created
+      const idItem = await createItem(kd_item, nm_item, "", 0, age_item, sort_item);
+      expect(idItem.length).toBe(14);
+
+      const item = await getItemById(idItem);
+
+      // expect item information must be matched
+      expect(item.id).toBe(idItem);
+      expect(item.kd_item).toBe(kd_item);
+      expect(item.nm_item).toBe(nm_item);
+      expect(item.age_item).toBe(age_item);
+      expect(item.sort_item).toBe(sort_item);
+    }
+  });
+
 });
